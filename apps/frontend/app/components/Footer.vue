71d25069f65e66eda2aaf04b2ffa058b85d30329
Huba Trifusz <huba.trifusz@gmail.com>
2025-12-14 18:23:39 +0100

    feat: update footer component with contact information and privacy links

---
+++ apps/frontend/app/components/Footer.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+import type { FooterColumn } from '@nuxt/ui';
+
+const columns: FooterColumn[] = [
+  {
+    label: 'Elérhetőség',
+    children: [
+      {
+        label: 'Email',
+        to: 'mailto:info@atlasride.hu',
+        icon: 'i-lucide-mail',
+      },
+      {
+        label: 'Telefon',
+        to: 'tel:+36706005522',
+        icon: 'i-lucide-phone',
+      },
+      {
+        label: 'Facebook',
+        to: 'https://www.facebook.com/profile.php?id=61571633512337',
+        target: '_blank',
+        icon: 'i-lucide-facebook',
+      },
+    ],
+  },
+  {
+    label: 'Adatkezelés és biztonság',
+    children: [
+      {
+        label: 'Süti beállítások',
+        icon: 'i-lucide-cookie',
+      },
+      {
+        label: 'Adatvédelmi tájékoztató',
+        icon: 'i-lucide-shield-half',
+      },
+    ],
+  },
+];
+</script>
+
+<template>
+  <UFooter class="pt-8 px-4">
+    <template #bottom>
+      <p class="text-muted text-sm">Copyright © {{ new Date().getFullYear() }} Atlas Ride</p>
+    </template>
+    <template #default>
+        <UFooterColumns :columns="columns" />
+    </template>
+  </UFooter>
+</template>
