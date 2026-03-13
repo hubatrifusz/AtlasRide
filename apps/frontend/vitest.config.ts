fff0f29835f26faa93355739947916a2c7c52727
Huba Trifusz <huba.trifusz@gmail.com>
2026-03-13 16:17:24 +0100

    chore (tests): configure vitest and playwright to only use their own test folder

--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -1,16 +1,17 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vitest/config';
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [vue()],
   test: {
     environment: 'happy-dom',
     globals: true,
+    include: ['tests/unit/**/*.test.ts'],
   },
   resolve: {
     alias: {
       '~': fileURLToPath(new URL('./app', import.meta.url)),
-    }
-  }
+    },
+  },
 });
